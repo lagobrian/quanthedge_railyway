4180c56f3ada754b541e6722584a5294ccb92b03
James Lago <lagobrian@outlook.com>
2026-03-23 00:41:16 +0300

    Add Plotly type declarations to fix build

---
+++ src/types/plotly.d.ts
@@ -0,0 +1,18 @@
+declare module 'react-plotly.js' {
+  import { Component } from 'react';
+  import Plotly from 'plotly.js-dist-min';
+
+  interface PlotParams {
+    data: Plotly.Data[];
+    layout?: Partial<Plotly.Layout>;
+    config?: Partial<Plotly.Config>;
+    style?: React.CSSProperties;
+    useResizeHandler?: boolean;
+    onInitialized?: (figure: any, graphDiv: HTMLElement) => void;
+    onUpdate?: (figure: any, graphDiv: HTMLElement) => void;
+  }
+
+  export default class Plot extends Component<PlotParams> {}
+}
+
+declare module 'plotly.js-dist-min';
